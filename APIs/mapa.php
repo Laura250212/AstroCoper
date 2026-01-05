6927c6adfb3305d347c2eaa15ef83a4e224bc079
Laura250212 <160271038+Laura250212@users.noreply.github.com>
2026-01-05 20:46:21 +0100

    mapa corregido

--- APIs/mapa.php
+++ APIs/mapa.php
@@ -1,11 +1,12 @@
 <?php
 $page_title = "Mapa Estelar - AstroCoper";
-include_once __DIR__ . '/../General/header.php';
+include_once __DIR__ . '/../Vistas/General/header.php';
 ?>
 
-<link rel="stylesheet" href="https://aladin.u-strasbg.fr/AladinLite/api/v3/latest/aladin.min.css" />
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="https://aladin.u-strasbg.fr/AladinLite/api/v3/latest/aladin.min.js"></script>
+
+<link rel="stylesheet" href="https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css" />
+<script type="text/javascript" src="https://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js"></script>
 
 <div class="store-container" style="text-align: center;">
     <h2 class="store-title">EXPLORADOR DEL COSMOS</h2>
@@ -14,15 +15,13 @@ include_once __DIR__ . '/../General/header.php';
     <div id="aladin-lite-div" style="width: 100%; height: 600px; margin: 0 auto; border: 2px solid #00ffff; box-shadow: 0 0 20px rgba(0,255,255,0.3);"></div>
     
     <script type="text/javascript">
-        let aladin;
-        A.init.then(() => {
-            aladin = A.aladin('#aladin-lite-div', {
-                survey: "P/DSS2/color", //Encuesta visual a color
-                fov: 60, //Campo de visión inicial
-                target: "M42" //Empieza mirando a la Nebulosa de Orión
-            });
+        //Versión 2
+        var aladin = A.aladin('#aladin-lite-div', {
+            survey: "P/DSS2/color", //Encuesta visual a color
+            fov: 60,                //Campo de visión inicial (Zoom)
+            target: "M42"           //Empieza enfocando la Nebulosa de Orión
         });
     </script>
 </div>
 
-<?php include_once __DIR__ . '/../General/footer.php'; ?>
+<?php include_once __DIR__ . '/../Vistas/General/footer.php'; ?>
